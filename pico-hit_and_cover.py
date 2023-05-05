92085227315607865fe9b123f69aa740b3ee8468
masHolic <85645607+masHolic@users.noreply.github.com>
2023-05-05 18:44:02 +0900

    pep8 compliant

--- pico-hit_and_cover.py
+++ pico-hit_and_cover.py
@@ -1,6 +1,7 @@
 import random
 from pimoroni import Button
-from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY, PEN_RGB332
+# from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY, PEN_RGB332
+from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY
 import jpegdec
 
 display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, rotate=0)
@@ -13,20 +14,21 @@ button_b = Button(13)
 button_x = Button(14)
 button_y = Button(15)
 
+
 class Hand:
     def __init__(self, side: str):
         self.side = side
         if self.side == 'left':
             self.small_x = 30
-            self.small_y1 =12 
-            self.small_y2 =52 
-            self.small_y3 =92 
+            self.small_y1 = 12
+            self.small_y2 = 52
+            self.small_y3 = 92
             self.img_s = ['img/gu_l_s.jpg', 'img/ti_l_s.jpg', 'img/pa_l_s.jpg']
 #            self.img_s[0] = 'img/gu_l_s.jpg'
 #            self.img_s[1] = 'img/ti_l_s.jpg'
 #            self.img_s[2] = 'img/pa_l_s.jpg'
             self.large_x = 40
-            self.large_y =32 
+            self.large_y = 32
             self.img_l = ['img/gu_l_l.jpg', 'img/ti_l_l.jpg', 'img/pa_l_l.jpg']
 #            self.img_l[0] = 'img/gu_l_l.jpg'
 #            self.img_l[1] = 'img/ti_l_l.jpg'
@@ -34,29 +36,29 @@ class Hand:
             self.line_a = [0, 2, 4, 2]
             self.line_b = [2, 4, 2, 0]
             self.line_c = [4, 2, 0, 2]
-            self.count_a = [  0,  5, 10, 15, 20, 25, 30, 35, 30, 25, 20, 15, 10,  5,  0,
-                              0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                              0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
-            self.count_b = [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                              0,  5, 10, 15, 20, 25, 30, 35, 30, 25, 20, 15, 10,  5,  0,
-                              0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
-            self.count_c = [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                              0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                              0,  5, 10, 15, 20, 25, 30, 35, 30, 25, 20, 15, 10,  5,  0]
-            self.even_shift = [ 0, 2, 4, 6, 8, 10, 8, 6, 4, 2, 0, 2, 4, 6, 8, 10, 8, 6, 4, 2, 0]
-            self.win_shift = [ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
-            self.lose_shift = [ 0, 0, 0, 0, 0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-10,-10,-10,-10,-10,-10]
+            self.count_a = [0,  5, 10, 15, 20, 25, 30, 35, 30, 25, 20, 15, 10, 5,  0,
+                            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,  0,
+                            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,  0]
+            self.count_b = [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,  0,
+                            0,  5, 10, 15, 20, 25, 30, 35, 30, 25, 20, 15, 10, 5,  0,
+                            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,  0]
+            self.count_c = [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,  0,
+                            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,  0,
+                            0,  5, 10, 15, 20, 25, 30, 35, 30, 25, 20, 15, 10, 5,  0]
+            self.even_shift = [0, 2, 4, 6, 8, 10, 8, 6, 4, 2, 0, 2, 4, 6, 8, 10, 8, 6, 4, 2, 0]
+            self.win_shift = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+            self.lose_shift = [0, 0, 0, 0, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -10, -10, -10, -10, -10, -10]
         elif self.side == 'right':
             self.small_x = 170
-            self.small_y1 =92 
-            self.small_y2 =52 
-            self.small_y3 =12 
+            self.small_y1 = 92
+            self.small_y2 = 52
+            self.small_y3 = 12
             self.img_s = ['img/gu_r_s.jpg', 'img/ti_r_s.jpg', 'img/pa_r_s.jpg']
 #            self.img_s[0] = 'img/gu_r_s.jpg'
 #            self.img_s[1] = 'img/ti_r_s.jpg'
 #            self.img_s[2] = 'img/pa_r_s.jpg'
             self.large_x = 130
-            self.large_y =32 
+            self.large_y = 32
             self.img_l = ['img/gu_r_l.jpg', 'img/ti_r_l.jpg', 'img/pa_r_l.jpg']
 #            self.img_l[0] = 'img/gu_r_l.jpg'
 #            self.img_l[1] = 'img/ti_r_l.jpg'
@@ -64,18 +66,18 @@ class Hand:
             self.line_a = [-0, -2, -4, -2]
             self.line_b = [-2, -4, -2, -0]
             self.line_c = [-4, -2, -0, -2]
-            self.count_a = [ -0, -5,-10,-15,-20,-25,-30,-35,-30,-25,-20,-15,-10,-5, -0,
-                             -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
-                             -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0]
-            self.count_b = [ -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
-                             -0, -5,-10,-15,-20,-25,-30,-35,-30,-25,-20,-15,-10, -5, -0,
-                             -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,  0]
-            self.count_c = [ -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
-                             -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
-                             -0, -5,-10,-15,-20,-25,-30,-35,-30,-25,-20,-15,-10, -5, -0]
-            self.even_shift = [-0,-2,-4,-6,-8,-10,-8,-6,-4,-2,-0,-2,-4,-6,-8,-10,-8,-6,-4,-2,-0]
-            self.win_shift = [-0,-2,-4,-6,-8,-10,-12,-14,-16,-18,-20,-22,-24,-26,-28,-30,-32,-34,-36,-38,-40]
-            self.lose_shift = [ 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10]
+            self.count_a = [-0, -5, -10, -15, -20, -25, -30, -35, -30, -25, -20, -15, -10, -5, -0,
+                            -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
+                            -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0]
+            self.count_b = [-0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
+                            -0, -5, -10, -15, -20, -25, -30, -35, -30, -25, -20, -15, -10, -5, -0,
+                            -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,  0]
+            self.count_c = [-0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
+                            -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0, -0,
+                            -0, -5, -10, -15, -20, -25, -30, -35, -30, -25, -20, -15, -10, -5, -0]
+            self.even_shift = [-0, -2, -4, -6, -8, -10, -8, -6, -4, -2, -0, -2, -4, -6, -8, -10, -8, -6, -4, -2, -0]
+            self.win_shift = [-0, -2, -4, -6, -8, -10, -12, -14, -16, -18, -20, -22, -24, -26, -28, -30, -32, -34, -36, -38, -40]
+            self.lose_shift = [0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10]
         self.status = 'wait'
         self.ready_count = 0
         self.line_a = [0, 2, 4, 2]
@@ -175,7 +177,7 @@ class Hand:
                 if self.countdown_count >= 45:
                     self.countdown_count = 0
                     self.get_result()
-        
+
         elif self.is_show():
             display_image(self.img_l[self.sign], self.large_x, self.large_y)
 
@@ -202,23 +204,30 @@ class Hand:
 
     def get_result(self):
         self.hand_dic = ['gu', 'ti', 'pa']
-        self.sign = random.randint(0,2)
+        self.sign = random.randint(0, 2)
         self.status = 'show'
         print(f'side:[{self.side}, {self.status}, {self.sign}]')
 
+
 WHITE = display.create_pen(255, 255, 255)
+
+
 def clear():
     display.set_pen(WHITE)
     display.clear()
     display.update()
+
+
 clear()
 
 # Create a new JPEG decoder for our PicoGraphics
 j = jpegdec.JPEG(display)
 
-def display_image(filename: str, x: int, y:int):
+
+def display_image(filename: str, x: int, y: int):
     j.open_file(filename)
     j.decode(x, y, jpegdec.JPEG_SCALE_FULL)
+
 
 def show_icon():
     display_image("img/sheld_l_s.jpg", 0, 10)
@@ -226,13 +235,14 @@ def show_icon():
     display_image("img/sheld_r_s.jpg", 210, 95)
     display_image("img/sword_r_s.jpg", 210, 10)
 
+
 show_icon()
 
 left_hand = Hand('left')
 right_hand = Hand('right')
 
 while True:
-    display.rectangle(30,0,180,135)
+    display.rectangle(30, 0, 180, 135)
     if button_a.read():
         left_hand.push_button2()
     elif button_b.read():
